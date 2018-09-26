2140319a94fe2833c5d1b5af8bf5b12aac3f9483
Jesse <jesse.hughes.it@gmail.com>
2018-09-26 12:28:36 -0700

    Shuffling scripts into scripts/ directory

---
+++ make.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+if [ "$WIN32" == "1" ]; then
+  echo " -- Building Win32"
+  rm ../bin/Debug/*.pdb
+  msbuild.exe /nologo /v:m ../bin/Bonsai.sln
+
+else # Win32
+  echo " -- Building Linux"
+  cd build
+  make test_collada "$@" 2>&1
+fi
+
+
---
+++ run_tests.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+TESTS_PASSED=0
+
+for test_executable in $(find bin/test*); do
+  if  $test_executable && [ "$TESTS_PASSED" == "0" ]; then
+    TESTS_PASSED=0
+  else
+    TESTS_PASSED=1
+  fi
+done
+
+
+if [ "$TESTS_PASSED" -eq 0 ]; then
+  echo ""
+  echo "All Tests Passed"
+else
+  echo ""
+  echo "One or more failures.  Inspect log for details."
+fi
+
+exit "$TESTS_PASSED"
