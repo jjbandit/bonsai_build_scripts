5dce51af1b712c55c4a0db28f3a193cf1be1f6d0
Jesse Hughes <jesse.hughes.it@gmail.com>
2022-08-20 11:22:00 -0600

    Random tweaking to scripts

--- sloc.sh
+++ sloc.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
 
-echo "Meta output : $(find poof/generated -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)"
-echo "Total SLOC  : $(find src -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)"
+META_SLOC=$(find poof/generated -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)
+TOTAL_SLOC=$(find poof -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)
+
+echo ""
+echo " Hand written   $(( $TOTAL_SLOC - $META_SLOC ))"
+echo " Meta output    $META_SLOC"
+echo "--------------------------"
+echo " Total          $TOTAL_SLOC sloc"
+echo ""
