6cc56333389d826646cb541162b749280614f901
Jesse Hughes <jesse.hughes.it@gmail.com>
2022-09-09 14:36:50 -0600

    Remove COLORFLAG

--- preamble.sh
+++ preamble.sh
@@ -3,11 +3,7 @@
 . scripts/set_platform.sh
 . scripts/check_if_terminal_interactive.sh
 
-COLORFLAG=""
-
 if [ $RunningInInteractiveTerminal == 1 ]; then
   . scripts/colors.sh
-else
-  COLORFLAG="--colors-off"
 fi
 
