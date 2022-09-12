1baf2f286e20db28f979ba24005859b769db4397
Jesse Hughes <jesse.hughes.it@gmail.com>
2022-09-12 16:10:18 -0600

    Tweak sloc

--- sloc.sh
+++ sloc.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 
-META_SLOC=$(find poof/generated -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)
-TOTAL_SLOC=$(find poof -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)
+META_SLOC=$(find generated   -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)
+PRNT_SLOC=$(find debug_print -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)
+DEPS_SLOC=$(find include     -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)
+POOF_SLOC=$(find poof        -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)
 
 echo ""
-echo " Hand written   $(( $TOTAL_SLOC - $META_SLOC ))"
+echo " Hand written   $POOF_SLOC"
 echo " Meta output    $META_SLOC"
+echo " Debug print    $PRNT_SLOC"
+echo " Deps           $DEPS_SLOC"
 echo "--------------------------"
-echo " Total          $TOTAL_SLOC sloc"
+echo " Total          $(( POOF_SLOC + META_SLOC + DEPS_SLOC + PRNT_SLOC )) sloc"
 echo ""
