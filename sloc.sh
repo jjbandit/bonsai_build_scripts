f3541c201aa69389710de3ea06d7a0126af880de
a pirate <jesse.hughes.it@gmail.com>
2023-08-13 18:54:58 -0600

    Update sloc.sh

--- sloc.sh
+++ sloc.sh
@@ -1,15 +1,17 @@
 #! /bin/bash
 
+SRC_SLOC=$( find src         -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)
 META_SLOC=$(find generated   -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)
 PRNT_SLOC=$(find debug_print -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)
 DEPS_SLOC=$(find include     -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)
-POOF_SLOC=$(find poof        -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)
+EXMP_SLOC=$(find examples    -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)
 
 echo ""
-echo " Hand written   $POOF_SLOC"
+echo " Hand written   $SRC_SLOC"
 echo " Meta output    $META_SLOC"
 echo " Debug print    $PRNT_SLOC"
 echo " Deps           $DEPS_SLOC"
+echo " Examples       $EXMP_SLOC"
 echo "--------------------------"
-echo " Total          $(( POOF_SLOC + META_SLOC + DEPS_SLOC + PRNT_SLOC )) sloc"
+echo " Total          $(( SRC_SLOC + META_SLOC + DEPS_SLOC + PRNT_SLOC + EXMP_SLOC)) sloc"
 echo ""
