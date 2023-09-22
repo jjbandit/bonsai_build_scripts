51d1a7eece6055b2f33d953cf92695787a6df5eb
a pirate <jesse.hughes.it@gmail.com>
2023-09-22 06:57:55 -0600

    Update scripts/sloc.sh with `external` dir name

--- sloc.sh
+++ sloc.sh
@@ -3,7 +3,7 @@
 SRC_SLOC=$( find src         -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)
 META_SLOC=$(find generated   -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)
 PRNT_SLOC=$(find debug_print -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)
-DEPS_SLOC=$(find include     -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)
+DEPS_SLOC=$(find external    -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)
 EXMP_SLOC=$(find examples    -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)
 
 echo ""
