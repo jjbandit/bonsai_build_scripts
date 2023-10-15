1152d393e0e1b7a6cf5ed568be71dd25dbab997c
a pirate <jesse.hughes.it@gmail.com>
2023-10-15 16:19:27 -0600

    Add shaders to sloc.sh

--- sloc.sh
+++ sloc.sh
@@ -5,6 +5,7 @@ META_SLOC=$(find generated   -name \*.cpp -print -o -name \*.h -print | xargs ca
 PRNT_SLOC=$(find debug_print -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)
 DEPS_SLOC=$(find external    -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)
 EXMP_SLOC=$(find examples    -name \*.cpp -print -o -name \*.h -print | xargs cat | wc -l)
+SHDR_SLOC=$(find . -name \*.fragmentshader -print -o -name \*.vertexshader -print -o -name \*.glsl -print | xargs cat | wc -l)
 
 echo ""
 echo " Hand written   $SRC_SLOC"
@@ -12,6 +13,7 @@ echo " Meta output    $META_SLOC"
 echo " Debug print    $PRNT_SLOC"
 echo " Deps           $DEPS_SLOC"
 echo " Examples       $EXMP_SLOC"
+echo " Shaders        $SHDR_SLOC"
 echo "--------------------------"
-echo " Total          $(( SRC_SLOC + META_SLOC + DEPS_SLOC + PRNT_SLOC + EXMP_SLOC)) sloc"
+echo " Total          $(( SRC_SLOC + META_SLOC + DEPS_SLOC + PRNT_SLOC + EXMP_SLOC + SHDR_SLOC )) sloc"
 echo ""
